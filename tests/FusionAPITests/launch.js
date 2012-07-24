6df41c4763079083283b535279989761f4099147
Scott Halstvedt <shalstvedt@gmail.com>
2012-07-24 14:00:02 -0600

    chrome.appWindow -> chrome.app.window per upstream Canary change

--- tests/FusionAPITests/launch.js
+++ tests/FusionAPITests/launch.js
@@ -1,5 +1,5 @@
 chrome.experimental.app.onLaunched.addListener(function() {
-  chrome.appWindow.create('tests.html', {
+  chrome.app.window.create('tests.html', {
     width: 800,
     height: 600
   });
